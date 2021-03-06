dbe12debf332e6d5b6397a15f17449ecd3f8354d
John Ratana <jratana1@gmail.com>
2021-03-06 12:34:27 -0500

    commit tree 61838a3a9219a93c2714f08874c0015895fcf08b

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+import { createStore } from 'redux';
 import manageUsers from './reducers/manageUsers'
+import { Provider } from 'react-redux'; /* code change */
 
 
 // add imports and code
+const store = createStore(manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-
-ReactDOM.render(
-  // add imports and code
+ReactDOM.render(<Provider store={store}>
   <App />
-  // add imports and code
-  ,
-  document.getElementById('root')
-);
+</Provider>, document.getElementById('root'));
+
